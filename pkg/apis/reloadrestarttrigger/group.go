3a82243234e7f38c749e9dde38f679c253e53ca9
Hans Kristian Nordengen <hans.kristian.nordengen@gmail.com>
2020-05-21 22:36:30 +0200

    Inital version

---
+++ pkg/apis/reloadrestarttrigger/group.go
@@ -0,0 +1,6 @@
+// Package reloadrestarttrigger contains reloadrestarttrigger API versions.
+//
+// This file ensures Go source parsers acknowledge the reloadrestarttrigger package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package reloadrestarttrigger
